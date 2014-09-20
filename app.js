59d2e6c33ff42fbda047c659fe560c3688e77a2e
Collin Glass <collinglass@gmx.com>
2014-09-20 17:57:00 -0400

    update hostname

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 var tenso  		= require( "tenso" ),
-	routes 		= require( "./routes" );
+	routes 		= require( "./routes" ),
+	dev			= true,
+	hostname,
+	port;
 
+if (dev) {
+	hostname = "localhost";
+	port = "8000";
+} else {
+	hostname = "pacific-sands-9126.herokuapp.com";
+	port = process.env.PORT;
+}
 
 var config = {
 	routes: routes,
-	hostname: "pacific-sands-9126.herokuapp.com",
-	port: process.env.PORT
+	hostname: hostname,
+	port: port
 }
 
 tenso( config );
 
 console.log("API engaged! cling! clang! bvvvvv!...");
+
